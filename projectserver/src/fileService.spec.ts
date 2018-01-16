b38b8624a1368a95c05162b970313d4e5043ef68
Adrian O'Grady <adriano@microsoft.com>
2018-01-16 10:16:23 -0800

    Initial work on actually getting file info

--- projectserver/src/fileService.spec.ts
+++ projectserver/src/fileService.spec.ts
@@ -1,16 +1,40 @@
-import FileService from './fileService';
+import FileService, { FileEntry, FileType } from './fileService';
 import { expect } from 'chai';
+import * as sinon from 'sinon';
+import * as fs from 'fs';
 import 'mocha';
+
+function fail(done: MochaDone) {
+    return (err) => {
+        done(err);
+    }
+}
+
+function fileEntry(name: string, type: FileType, size: number) {
+    return new FileEntry(name, type, size);
+}
 
 describe('FileService', () => {
 
-    it('should return the correct file list', () => {
+    beforeEach(() => {
+        let stub = sinon.stub(fs, 'readdir').callsFake((path, handler) => {
+            handler(null, [
+                'a.txt', 'b.bin', 'c.png'
+            ]);
+        });
+    })
+
+    it('should return the correct file list', (done) => {
         let fs = new FileService();
-        expect(fs.getFiles()).to.eql([
-            'a.txt',
-            'b.bin',
-            'c.png'
-        ]);
+        fs.getFileList('.')
+        .then((files) => {
+            expect(files).to.eql([
+                fileEntry('a.txt', FileType.File, 5),
+                fileEntry('b.bin', FileType.File, 7),
+                fileEntry('c.png', FileType.File, 11)
+            ]);
+        })
+        .then(done, fail(done));
     });
 
 });
--- projectserver/src/fileService.ts
+++ projectserver/src/fileService.ts
@@ -1,14 +1,90 @@
+import * as fs from 'fs';
+
+export enum FileType {
+    Unknown,
+    File,
+    Directory
+}
+
+export class FileEntry {
+    constructor(
+        public readonly name: string,
+        public readonly type: FileType,
+        public readonly size: number
+    ) {
+
+    }
+}
+
+function fileStatsToFileType(stats: fs.Stats): FileType {
+    if (stats.isFile()) {
+        return FileType.File;
+    }
+    else if (stats.isDirectory()) {
+        return FileType.Directory;
+    }
+    else {
+        return FileType.Unknown;
+    }
+}
+
 export default class FileService {
 
     public constructor() {
 
     }
 
-    public getFiles(): string[] {
-        return [
-            "a.txt",
-            "b.bin",
-            "c.png"
-        ];
+    private _getFileStats(path: string): Promise<fs.Stats> {
+        return new Promise<fs.Stats>((resolve, reject) => {
+            fs.stat(path, (err, result) => {
+                if (!!err) {
+                    reject(err);
+                }
+                else {
+                    resolve(result);
+                }
+            })
+        });
+    }
+
+    public getFileList(path: string): Promise<FileEntry[]> {
+        return new Promise<FileEntry[]>((resolve, reject) => {
+            fs.readdir(path, (err, files) => {
+                if (!!err) {
+                    reject(err);
+                }
+                else {
+                    let list: FileEntry[] = [];
+                    if (files.length == 0) {
+                        resolve(list);
+                        return;
+                    }
+
+                    let promise: Promise<void> = null;
+
+                    for (let i = 0; i < files.length; i++) {
+                        let p = this._getFileStats(path + '/' + files[i])
+                        .then((stats) => {
+                            list.push(new FileEntry(
+                                files[i],
+                                fileStatsToFileType(stats),
+                                stats.size
+                            ));
+                        }, reject);
+
+                        if (promise == null) {
+                            promise = p;
+                        }
+                        else {
+                            promise = promise.then(() => p);
+                        }
+                    }
+
+                    promise.then(() => {
+                        resolve(list);                        
+                    });
+                }
+            });
+        });
     }
 }
